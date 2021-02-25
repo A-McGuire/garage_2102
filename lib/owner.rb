68b1fda3ad3a9480fe97f5e5fdb7826b4fc39dd6
Aidan McGuire <>
2021-02-25 09:53:57 -0700

    Iteration 2 tests passing

--- lib/owner.rb
+++ lib/owner.rb
@@ -12,12 +12,17 @@ class Owner
     @cars
   end
 
-  def buy(car)
+  def buy(car) # todo can probably clean this up
     info_hash = {}
     info_arr = car.split
     info_hash[:year] = info_arr.shift
     info_hash[:description] = info_arr.join(" ")
-    
     @cars << Car.new(info_hash)
+  end
+  
+  def vintage_cars
+    @cars.find_all do |car|
+      car.age > 24
+    end
   end
 end
--- test/owner_test.rb
+++ test/owner_test.rb
@@ -8,6 +8,7 @@ class OwnerTest < MiniTest::Test
 
  def test_it_exists_with_attributes
     owner_1 = Owner.new('Regina George', 'Heiress')
+
     assert_instance_of Owner, owner_1
     
     assert_equal "Regina George", owner_1.name
@@ -22,25 +23,29 @@ class OwnerTest < MiniTest::Test
   
   def test_buy_car
     owner_1 = Owner.new('Regina George', 'Heiress')
+
     owner_1.buy('1967 Green Ford Mustang')
     
     assert_equal 1, owner_1.cars.count #todo refactor for greater accuracy
   end
+  
+  def test_buy_more_cars
+    owner_1 = Owner.new('Regina George', 'Heiress')
+
+    owner_1.buy('1967 Green Ford Mustang')
+    owner_1.buy('2001 Silver BMW 3-Series')
+    
+    assert_equal 2, owner_1.cars.count
+  end
+  
+  def test_buy_vintage_car
+    owner_1 = Owner.new('Regina George', 'Heiress')
+    
+    owner_1.buy('1967 Green Ford Mustang')
+    owner_1.buy('2001 Silver BMW 3-Series')
+    owner_1.buy('1963 Red Chevrolet Corvette')
+    
+    assert_equal 2, owner_1.vintage_cars.count
+  end
 end
 
-
-
-# # => #<Car:0x00007fcb13bd22d0...>
-
- 
-# owner_1.buy('2001 Silver BMW 3-Series')
-# # => #<Car:0x00007fb898081850...>
-
-# owner_1.cars
-# # => [#<Car:0x00007fcb13bd22d0...>, #<Car:0x00007fb898081850...>] 
-
-# owner_1.buy('1963 Red Chevrolet Corvette')
-# # => #<Car:0x00007fb898081850...> 
-
-# owner_1.vintage_cars
-# # => [#<Car:0x00007fcb13bd22d0...>, #<Car:0x00007fb898081850...>]
